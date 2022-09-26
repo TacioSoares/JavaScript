c5bee56d4abac0a8272d4fcc083117408d031750
Tácio Soares <taciofsoares@gmail.com>
2022-09-26 16:02:04 -0300

    condiççao

---
+++ curso-js/aula 12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 68
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade < 18) {
+    console.log('Voto opcional')
+} else if (idade > 67) {
+    console.log('Voto opcional')
+} else {
+    console.log ('Voto obrigatório')
+}
---
+++ curso-js/aula 12/ex012.js
@@ -0,0 +1,7 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são ${hora} horas.`)
+switch (var == 13) {
+    
+    break
+}
