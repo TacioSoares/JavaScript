0516e4ddff006b57df5a430bdc692794e817beb2
Tácio Soares <taciofsoares@gmail.com>
2022-09-26 13:36:26 -0300

    treinando

---
+++ curso-js/aula 11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60
+console.log (`A velocidade do seu carro é ${vel} por hora`)
+if (vel > 60) {
+    console.log(`Você está acima da velocidade, você será MUTADO!`)
+} else {
+    console.log(`Dirija com cuidado sempre e boa viagem`)
+}
